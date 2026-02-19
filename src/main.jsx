78f615e54352bd7bf059746851cbcec9ebb9a8f3
Douce1 <douce40@naver.com>
2026-02-19 11:16:12 +0900

    Router3

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Intel-React01">
       <App />
     </BrowserRouter>
   </StrictMode>,
